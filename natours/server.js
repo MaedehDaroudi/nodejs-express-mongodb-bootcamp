1d92ac59b0cd4b4997b11f2f3694a8594b303f90
Maedeh Daroudi <darrodi2017@gmail.com>
2023-04-12 12:22:25 +0330

    connect to Mongodb atlas database

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
+const mogoose = require('mongoose');
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mogoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connection successful.'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
