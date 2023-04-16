c5ba74522c376f1f43579b3801604ebcbaa5b29d
Maedeh Daroudi <darrodi2017@gmail.com>
2023-04-16 23:40:31 +0330

    Refactoring API Features

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -8,44 +8,91 @@ exports.aliasTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    const queryObj = { ...req.query };
+class APIFeature {
+  constructor(query, queryString) {
+    this.query = query;
+    this.querySting = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.querySting };
+    console.log('queryObj: ', queryObj);
     const excludedField = ['page', 'limit', 'fields', 'sort'];
     excludedField.forEach(el => delete queryObj[el]);
-    let querySting = JSON.stringify(queryObj);
-    querySting = querySting.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      match => `$${match}`
-    );
-    let query = Tour.find(JSON.parse(querySting));
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    console.log('this.query: ', this.query);
+    return this;
+  }
 
-    if (req.query.sort) {
-      const sortField = req.query.sort.split(',').join(' ');
-      query = query.sort(sortField);
-    } else query = query.sort('-createdAt');
+  sort() {
+    if (this.querySting.sort) {
+      const sortField = this.querySting.sort.split(',').join(' ');
+      this.query = this.query.sort(sortField);
+    } else this.query = this.query.sort('-createdAt');
+    return this;
+  }
 
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else query = query.select('-__v');
+  fields() {
+    if (this.querySting.fields) {
+      const fields = this.querySting.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else this.query = this.query.select('-__v');
+    return this;
+  }
 
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
+  paginate() {
+    const page = this.querySting.page * 1 || 1;
+    const limit = this.querySting.limit * 1 || 100;
     const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
 
-    query = query.skip(skip).limit(limit);
+exports.getAllTours = async (req, res) => {
+  try {
+    // const queryObj = { ...req.query };
+    // const excludedField = ['page', 'limit', 'fields', 'sort'];
+    // excludedField.forEach(el => delete queryObj[el]);
+    // let querySting = JSON.stringify(queryObj);
+    // querySting = querySting.replace(
+    //   /\b(gte|gt|lte|lt)\b/g,
+    //   match => `$${match}`
+    // );
+    // let query = Tour.find(JSON.parse(querySting));
 
-    if (req.query.page) {
-      const numTour = await Tour.countDocuments();
-      console.log('numTour: ', numTour);
-      console.log('skip: ', skip);
-      if (skip > numTour) {
-        throw new Error('this page not exist');
-      }
-    }
+    // if (req.query.sort) {
+    //   const sortField = req.query.sort.split(',').join(' ');
+    //   query = query.sort(sortField);
+    // } else query = query.sort('-createdAt');
 
-    const tours = await query;
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    // } else query = query.select('-__v');
+
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
+
+    // query = query.skip(skip).limit(limit);
+
+    // if (req.query.page) {
+    //   const numTour = await Tour.countDocuments();
+    //   console.log('numTour: ', numTour);
+    //   console.log('skip: ', skip);
+    //   if (skip > numTour) {
+    //     throw new Error('this page not exist');
+    //   }
+    // }
+    const features = new APIFeature(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .fields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
@@ -55,6 +102,7 @@ exports.getAllTours = async (req, res) => {
       }
     });
   } catch (err) {
+    console.log('err: ', err);
     res.status(404).json({
       status: 'fail',
       message: err
